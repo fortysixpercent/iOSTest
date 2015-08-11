0d795d3d56abfab57f7626273929ab9e88fe0a69
Austin Alter <aalter@aalter-macpro.local>
2015-08-11 15:14:06 -0400

    Initial Commit

---
+++ aanative/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aanative
+//
+//  Created by Austin Alter on 8/11/15.
+//  Copyright (c) 2015 Austin Alter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
